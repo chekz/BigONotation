9e93589ab05cba45a8a31879551b17c5ed1d3be3
chekz <ciangardiner1990@gmail.com>
2017-07-05 23:12:22 +0100

    All n algorithms are called by doComparisons now.

--- src/BigONotation/Main.java
+++ src/BigONotation/Main.java
@@ -3,7 +3,6 @@ package BigONotation;
 public class Main {
 	public static void main(String[] args) {
 		BigONotation bigO = new BigONotation();
-		bigO.nComparison(6);
-		bigO.nSquaredComparison(6);
+		bigO.doComparisons();
 	}
 }
